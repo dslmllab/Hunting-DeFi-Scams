ac768181224eec582eac92d935b3f987eec94bf8
dslmllab <146118938+dslmllab@users.noreply.github.com>
2024-05-04 23:57:22 -0400

    Delete Dataset directory

--- Dataset/vsmartcontracts/dangerous delegatecall (DE)/44311.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract DelegateCaller {
-    uint public n;
-    function delegatecallSetN(address _e, uint _n) public {
-        if (! _e.delegatecall(bytes4(keccak256("setN(uint256)")), _n))
-            revert();
-    }
-}
